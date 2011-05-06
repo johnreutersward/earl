e741c25b6f28bb53293e7a2f0c4d1a84a6f6d535
Simon Young <youngen.simon@gmail.com>
2011-05-06 11:07:05 +0200

    server changes

--- src/server.erl
+++ src/server.erl
@@ -1,15 +1,18 @@
 -module(server).
--export([init/0,server/0,runtest/0]).
+-export([init/0,server/0,runtest/0,msgSender/3]).
 -include_lib("eunit/include/eunit.hrl").
 
 init() ->
     register(db,spawn(database,init,[])),
-    register(theServer, spawn(server, server, [])).
+    register(theServer, self()),
+    server().
 
 server() ->
     io:format("Server pid: ~w~n", [self()]),
     receive
-	{setStatus, Pid, Alias, Status} ->  
+	{say,Msg,Pid,Alias} ->
+	    spawn(server,msgSender,[Msg,Pid,Alias]);
+	{setStatus, Pid, Alias,Status} ->  
 	    db ! {client,setStatus,Pid,Alias,Status};
 	{checkAlias, Pid, Alias} -> 
 	    db ! {checkAlias,Pid,Alias};	       	      
@@ -17,28 +20,43 @@ server() ->
 	    db ! {remove,Pid}	
     end,
     server().
-%% <<<<<<< HEAD
 
-checkAlias(Pid, Alias) ->
-    Answer = ets:match(clientTable, {'$1', Alias, '_'}),
-    if
-	Answer == [] -> 
-	    Pid ! aliasTrue;
-	true -> 
-	    Pid ! aliasFalse
+%% HELP FUNCTIONS %%
+
+
+%% @doc Gets the Status of the process with Process Id Pid
+
+getStatus(Pid) ->
+    db ! {getStatus,Pid,self()},
+    receive
+	{answer,Status} ->
+	    Status
     end.
 
-%% =======
-%% >>>>>>> 30d7f39f95bc91f38ebc00607e76614c10442b68
-%% Test cases
+%% @doc gets a list of all the processes with the status Status.
+
+getStatusList(Status) ->
+    db ! {getStatusList,Status,self()},
+    receive
+	{answer,StatusList} ->
+	    StatusList
+    end.
+	    
+%% @doc sends a message to all the PIDs in the List
+
+sendMsg([],_,_) -> ok;
+sendMsg([H | T],Alias,Msg) ->
+    H ! {message, Alias,Msg},
+    sendMsg(T,Alias,Msg).
+
+msgSender(Msg,Pid,Alias) ->
+    Status = getStatus(Pid),
+    StatusList = getStatusList(Status),
+    sendMsg(StatusList,Alias,Msg).
+
+% Test cases
 
 runtest() ->
     test(),
     init:stop().
-checkAlias_test() ->
-    ets:new(clientTable, [set, public, named_table]),
-    checkAlias(self(), "foo"),
-    ets:insert(clientTable, {self(), "foo", [main]}),
-    checkAlias(self(), "foo"),
-    checkAlias(self(), "bar").
 
