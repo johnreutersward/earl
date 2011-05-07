2349455874e6bc57d46a189d752219bdd9c87e8a
Simon Young <youngen.simon@gmail.com>
2011-05-07 11:38:49 +0200

    documentation and a receive statement

--- src/server.erl
+++ src/server.erl
@@ -1,3 +1,10 @@
+%% @author Tobias Ericsson <tobiasericsson90@hotmail.com>
+%% @author Andreas Hammar <andreashammar@gmail.com>
+%% @author John Reuterswärd <rojters@gmail.com>
+%% @author Simon Young <youngen.simon@gmail.com>
+%% @doc This is the main server that handles all the communication between
+%% clients and the database.
+
 -module(server).
 -export([init/0,server/0,runtest/0,msgSender/3]).
 -include_lib("eunit/include/eunit.hrl").
@@ -5,32 +12,36 @@
 init() ->
     register(srv, self()),
     register(db,spawn(database,init,[])),
-	io:format("-----------------------------------------------~n", []),
-	io:format("------  Earl Game Club server initiated  ------~n", []),
-	io:format("-----------------------------------------------~n", []),
-	io:format("         Host:   ~s~n", [net_adm:localhost()]),
-	io:format("         Node:   ~s~n", [node()]),
-	io:format("         Cookie: ~s~n", [erlang:get_cookie()]),
-	io:format("-----------------------------------------------~n~n", []),
+    io:format("-----------------------------------------------~n", []),
+    io:format("------  Earl Game Club server initiated  ------~n", []),
+    io:format("-----------------------------------------------~n", []),
+    io:format("         Host:   ~s~n", [net_adm:localhost()]),
+    io:format("         Node:   ~s~n", [node()]),
+    io:format("         Cookie: ~s~n", [erlang:get_cookie()]),
+    io:format("-----------------------------------------------~n~n", []),
     server().
 
 server() ->
     receive
-		{say, Msg, Pid, Alias} ->
-			io:format("Server: Received 'say', spawning message-sender~n", []),
-		    spawn(server, msgSender, [Msg, Pid, Alias]);
-		{setStatus, Pid, Alias,Status} ->  
-			io:format("Server: Received 'setStatus' request, forwarding to database~n", []),
-			db ! {setStatus, Pid, Alias, Status};
-		{checkAlias, Alias, Origin} -> 
-			io:format("Server: Received 'checkAlias', forwarding to db~n", []),
-			db ! {checkAlias, Alias, Origin};	       	      
-		{quit, Pid} ->
-			io:format("Server: Received 'quit' from pid, sending removal request to db~n", []),
-		    db ! {remove,Pid};
-		{debug, Msg} ->
-			io:format("~s~n", [Msg])
-	end,
+	{say, Msg, Pid, Alias} ->
+	    io:format("Server: Received 'say', spawning message-sender~n", []),
+	    spawn(server, msgSender, [Msg, Pid, Alias]);
+	{setStatus, Pid, Alias,Status} ->  
+	    io:format("Server: Received 'setStatus' request, forwarding to database~n", []),
+	    db ! {setStatus, Pid, Alias, Status};
+	{checkAlias, Alias, Origin} -> 
+	    io:format("Server: Received 'checkAlias', forwarding to db~n", []),
+	    db ! {checkAlias, Alias, Origin};	       	      
+	{quit, Pid} ->
+	    io:format("Server: Received 'quit' from pid, sending removal request to db~n", []),
+	    db ! {remove,Pid};
+	{debug, Msg} ->
+	    io:format("~s~n", [Msg]);
+	{getNumClients,Origin} ->
+	    io:format("server recevied 'getNumClients'",[]),
+	    db ! {getNumClients,Origin},
+	    
+    end,
     server().
 
 %% HELP FUNCTIONS %%
@@ -50,10 +61,10 @@ getStatus(Pid) ->
 getStatusList(Status) ->
     db ! {getStatusList, Status, self()},
     receive
-		{answer, StatusList} ->
-		    StatusList
+	{answer, StatusList} ->
+	    StatusList
     end.
-	    
+
 %% @doc sends a message to all the PIDs in the List
 
 sendMsg([], _, _) -> ok;
