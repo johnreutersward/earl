29f954a25f5a5a1ba9de7c8220d930803b41214e
Tobias Ericsson <tobiasericsson90@hotmail.com>
2011-05-16 16:05:40 +0200

    Game API finished

--- src/gameAPI.erl
+++ src/gameAPI.erl
@@ -1,13 +1,51 @@
 
 -module(gameAPI).
--export([]).
+-export([init/2, getInput/1, print/2]).
 
-%% 
-% @doc Initializes the game
-% GameRoomPid -> atom() - Process id of game room
-% Players -> [Pid1, ..., Pidn] - Game players
-%
-init(GameRoomPid, Players) ->
-	ok.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% GameAPI functions 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+init(Game, Players) ->
+	State = Game:init(),
+	run(Game, State, Players, []).
+
+run(Game, State, Players, []) ->
+	run(Game, State, Players, Players);
+run(Game, State, Players, [NextPlayer | RemainingPlayers]) ->
+	case Game:checkFinished(State) of
+		{true, Winner} ->
+			finish(Winner, Players);
+		{false} ->
+			State = Game:nextTurn(NextPlayer),
+			run(Game, State, Players, RemainingPlayers)
+	end.
+
+finish({_, WinnerAlias, _}, Players) ->
+	print("The winner is "++WinnerAlias++"! Congratulations!\n", Players).
+
+
+getInput(Pid) ->
+	Pid ! {input},
+	receive
+		{input, Input} ->
+			Input
+	end.
+
+print(_, []) ->
+	ok;
+print(Output, [Player | Players]) ->
+	Player ! {output, Output},
+	print(Output, Players).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  Functions to implement in Games
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% init() -> State
+
+% checkFinished(State) -> Winner
+
+% nextTurn(NextPlayer) -> State
 
