45c8c2e4fcdc80dbaeb596585b03720d7c7f0354
Tobias Ericsson <tobiasericsson90@hotmail.com>
2011-05-16 14:52:19 +0200

    start of gameAPI

---
+++ src/gameAPI.erl
@@ -0,0 +1,13 @@
+
+-module(gameAPI).
+-export([]).
+
+%% 
+% @doc Initializes the game
+% GameRoomPid -> atom() - Process id of game room
+% Players -> [Pid1, ..., Pidn] - Game players
+%
+init(GameRoomPid, Players) ->
+	ok.
+
+
