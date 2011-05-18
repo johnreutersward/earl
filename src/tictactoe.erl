6a42e72ab7be10af7ac42bb2e940f5c3d382c5e4
Simon Young <youngen.simon@gmail.com>
2011-05-18 12:40:46 +0200

    joooohn

--- src/tictactoe.erl
+++ src/tictactoe.erl
@@ -239,7 +239,7 @@ printState({_,[a,A1,A2,A3],[b,B1,B2,B3],[c,C1,C2,C3]},Players) ->
     print("====1===2===3====\n",Players),
     print("A % " ++ A1 ++ " % " ++ A2 ++ " % " ++ A3 ++ " %\n",Players),
     print("B % " ++ B1 ++ " % " ++ B2 ++ " % " ++ B3 ++ " %\n",Players),
-    print("C % " ++ C1 ++ " % " ++ C2 ++ " % " ++ C3,++ " %\n"Players),
+    print("C % " ++ C1 ++ " % " ++ C2 ++ " % " ++ C3 ++ " %\n",Players),
     print("================================\n",Players).
     
 
