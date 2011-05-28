6b704cdfd84767d6631fa12691b96a6b98a7e486
rojters <rojters@gmail.com>
2011-05-28 19:02:24 +0200

    fixed formating

--- src/guess_the_number.erl
+++ src/guess_the_number.erl
@@ -4,30 +4,31 @@
 init(Players) -> 
     random:seed(erlang:now()),
     The_number = random:uniform(100),
-    Message = "Welcome to Guess The Number! Guess a number between 0 and 100.",
+    Message = "\nWelcome to Guess The Number! Guess a number between 0 and 100.\n",
     gameAPI:print(Message,Players),
 	{gameInProgress, The_number, {noPid, noPlayer}}.
 
 nextTurn({_,The_number,_}, {PlayerPid, PlayerAlias}, Players) ->
-	gameAPI:print("Your turn", [{PlayerPid, PlayerAlias}]),
+    Remaining = lists:keydelete(PlayerPid, 1, Players),
+	gameAPI:print("Your turn!", [{PlayerPid, PlayerAlias}]),
+	gameAPI:print(PlayerAlias ++ "'s turn", Remaining),
     gameAPI:print("Guess the number: ", [{PlayerPid, PlayerAlias}]),
     The_guess = (gameAPI:getNumber(PlayerPid)),
-    Remaining = lists:keydelete(PlayerPid, 1, Players),
 	if
 		is_pid(The_guess) ->
 			gameAPI:print(element(2,lists:keyfind(The_guess, 1, Players)) ++ " has left the game", Players),
 			{gameOver, The_number, hd(Remaining)};
 		The_guess == The_number ->
 			gameAPI:print(PlayerAlias ++ " guessed " ++ integer_to_list(The_guess), Remaining),
-			gameAPI:print("That's the right number!!", Players),
+			gameAPI:print("That's the right number!!\n", Players),
 			{gameOver, The_number, {PlayerPid, PlayerAlias}};
 		The_guess < The_number ->
 			gameAPI:print(PlayerAlias ++ " guessed " ++ integer_to_list(The_guess), Remaining),
-			gameAPI:print("The number is bigger!", Players),
+			gameAPI:print("The number is bigger!\n", Players),
 			{gameInProgress, The_number, {PlayerPid, PlayerAlias}};
 		true ->
 			gameAPI:print(PlayerAlias ++ " guessed " ++ integer_to_list(The_guess), Remaining),
-			gameAPI:print("The number is smaller!", Players),
+			gameAPI:print("The number is smaller!\n", Players),
 			{gameInProgress, The_number, {PlayerPid, PlayerAlias}}   
     end.
 
