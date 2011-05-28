d7e3ebc42b7395a3fcaedfbf4fde3226f0a4818a
Andreas Hammar <andreashammar@ymail.com>
2011-05-28 11:53:36 +0200

    Added clientCrash functionality in guess the number

--- src/guess_the_number.erl
+++ src/guess_the_number.erl
@@ -15,25 +15,16 @@ nextTurn({_,The_number,_},{Next_player_Pid,Next_player_Alias},Players) ->
     gameAPI:print("guess the number: ",[{Next_player_Pid,Next_player_Alias}]),
     The_guess = (gameAPI:getNumber(Next_player_Pid)),
     Remaining= lists:keydelete(Next_player_Pid,1,Players),
-	if
-		%%is_pid(The_guess) ->
-		%%	gameAPI:print("Pid", Players);
-		is_integer(The_guess) ->
-			gameAPI:print("Integer", Players);
-		is_atom(The_guess) ->
-			gameAPI:print("Atom", Players)
-	end,
 	if 
-		is_integer(The_guess) ->		
-			gameAPI:print(Next_player_Alias++" guessed "++integer_to_list(The_guess)++"\n",Remaining)
+		is_number(The_guess) ->		
+			gameAPI:print(Next_player_Alias++" guessed "++integer_to_list(The_guess)++"\n",Remaining);
+		true ->
+			ok
 	end,
-	gameAPI:print("Förbi Print", Players),
 	if
 		is_pid(The_guess) ->
-			gameAPI:print("Här!!", Players),
-			%%gameAPI:print(element(2,lists:keyfind(The_guess, 1, Players)) ++ "has left the game", Players),
-			gameAPI:print("Player left", Players),
-			{1, The_number, {Next_player_Pid, Next_player_Alias}};
+			gameAPI:print(element(2,lists:keyfind(The_guess, 1, Players)) ++ " has left the game", Players),
+			{1, The_number, hd(Remaining)};
 		The_guess == The_number ->
 			gameAPI:print("That's the right number!!\n", Players),
 			{1, The_number, {Next_player_Pid, Next_player_Alias}};
